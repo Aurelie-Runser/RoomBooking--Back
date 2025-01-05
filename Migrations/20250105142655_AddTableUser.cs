dc2f0268475cca19ebb7422bcc6902e94bbcdee0
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2025-01-05 15:43:04 +0100

    mise à jour de la bdd (modif Rooms + ajout Users)

---
+++ Migrations/20250105142655_AddTableUser.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationDeSalle__Back.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Slug",
+                table: "Rooms",
+                newName: "AdressComplements");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Lastname = table.Column<string>(type: "TEXT", nullable: false),
+                    Firstname = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Company = table.Column<string>(type: "TEXT", nullable: false),
+                    Job = table.Column<string>(type: "TEXT", nullable: false),
+                    Type = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "AdressComplements",
+                table: "Rooms",
+                newName: "Slug");
+        }
+    }
+}
