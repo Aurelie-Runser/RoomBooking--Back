6808764edc3b1d2f348d4338a93ac50ef84f8ac4
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2025-01-13 09:48:51 +0100

    suppression groupe salle "vide"

--- Validations/Class/RoomGroupeClass.cs
+++ Validations/Class/RoomGroupeClass.cs
@@ -3,9 +3,9 @@ using System.ComponentModel.DataAnnotations;
 namespace RoomBookingApi.Validations {
 
     public static class Groupes {
-        public static readonly string[] AllowedGroupes = { 
-            "", "Petite Réunion", "Moyenne Réunion", "Grande Réunion", 
-            "Conférence", "Fête", "Gala" 
+        public static readonly string[] AllowedGroupes = {
+            "Petite Réunion", "Moyenne Réunion", "Grande Réunion",
+            "Conférence", "Fête", "Gala"
         };
     }
 }
