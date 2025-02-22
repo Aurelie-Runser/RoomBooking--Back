d0ee4478cd302f860510ab61553d5931bd615b8d
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2025-02-22 19:13:46 +0100

    changement format image des room (string => binaire)

---
+++ Migrations/20250222165308_ChageTypeColumnPictureInTableRoom.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationDeSalle__Back.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChageTypeColumnPictureInTableRoom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Picture",
+                table: "Rooms",
+                type: "BLOB",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Picture",
+                table: "Rooms",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(byte[]),
+                oldType: "BLOB",
+                oldNullable: true);
+        }
+    }
+}
