a87100311068ed0e67a49d9ff52b3ae8b8f6757b
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2025-02-26 09:22:02 +0100

    refactoring : Auth (inscription, connexion, profil)

--- Models/Auth.cs
+++ Models/Auth.cs
@@ -3,9 +3,17 @@ using System.ComponentModel.DataAnnotations;
 namespace RoomBookingApi.Models
 {
 
-    public record LoginRequest
+    public record Login
     {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public required string Email { get; set; }
+
+        [Required]
         public required string Password { get; set; }
     }
-}
+
+    public record Register: User
+    {
+    }
+}   
